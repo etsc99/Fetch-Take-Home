4a6f399aed3ced18b481df3c7b10fe9e370c4463
etsc99 <ethan.schacht99@gmail.com>
2025-02-01 19:52:38 -0700

    2/1

---
+++ part2.sql
@@ -0,0 +1,7 @@
+/* Part 2: SQL Queries */
+
+-- What are the top 5 brands by receipts scanned among users 21 and over?
+
+-- What is the percentage of sales in the Health & Wellness category by generation?
+
+-- At what percent has Fetch grown year over year?
